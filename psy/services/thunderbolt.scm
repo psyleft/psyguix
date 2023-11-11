af5e548b409f7ee2ae8bd36f5f688dd2839b3054
Connor Clark <connor@psyleft.com>
2023-11-11 14:34:02 -0500

    Fully remove bolt shepherd service
    The bolt documentation states that dbus should start the daemon when it is
    needed. Cursory testing indicates this works on Guix.

--- psy/services/thunderbolt.scm
+++ psy/services/thunderbolt.scm
@@ -9,7 +9,6 @@
   #:use-module (gnu services)
   #:use-module (gnu services configuration)
   #:use-module (gnu services dbus)
-  #:use-module (gnu services shepherd)
 
   #:export (bolt-service-type
             bolt-configuration))
@@ -19,27 +18,13 @@
     (package bolt)
     "Package to use for bolt"))
 
-(define (bolt-shepherd-service cfg)
-  (match-record cfg <bolt-configuration>
-                (package)
-    (list (shepherd-service
-           (documentation "Runs bolt")
-           (provision '(bolt))
-           (requirement '(dbus-system))
-           (start #~(make-forkexec-constructor
-                     (list #$(file-append package "/libexec/boltd"))
-                     #:log-file "/var/log/bolt.log"))
-           (stop #~(make-kill-destructor))))))
-
 (define bolt-service-type
   (service-type
    (name 'bolt)
    (extensions
     (list (service-extension dbus-root-service-type
                              (compose list bolt-configuration-package))
-          ;; (service-extension shepherd-root-service-type
-          ;;                    bolt-shepherd-service)
           (service-extension profile-service-type
                              (compose list bolt-configuration-package))))
    (default-value (bolt-configuration))
-   (description "Provides bolt thunderbolt management tool")))
+   (description "Provides the bolt thunderbolt management tool")))
